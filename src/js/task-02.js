742d0d74977f0c095ad44bf72701c833328d9cc8
Alexandr Glazunov <matveychuk1998@gmail.com>
2023-01-08 00:00:00 +0200

    Сompleted tasks

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listEl = document.getElementById("ingredients");
+const ingredientArray = [];
+
+for(const ingredient of ingredients){
+ const createEl = document.createElement("li");
+ createEl.textContent = ingredient;
+  createEl.classList.add("item");
+  ingredientArray.push(createEl);
+};
+
+ listEl.append(...ingredientArray); 
